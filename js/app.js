c98bfdbe2980835653a35629b4b0079b1f117510
Agata Jones <agata.k.jones@gmail.com>
2021-04-14 11:35:58 +0100

    Finish tutorial

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
+(function () {
+
 const quotesEl = document.querySelector('.quotes');
 const loader = document.querySelector('.loader');
 
+// get the quotes from the API
 const getQuotes = async (page, limit) => {
     const API_URL = `https://api.javascripttutorial.net/v1/quotes/?page=${page}&limit=${limit}`;
     const response = await fetch(API_URL);
@@ -14,20 +17,21 @@ const getQuotes = async (page, limit) => {
 
 }
 
-const showQuotes = (quotes) => {
-    quotes.forEach(quote => {
-        const quoteEl = document.createElement('blockquote');
-        quoteEl.classList.add('quote');
+    // show the quotes
+    const showQuotes = (quotes) => {
+        quotes.forEach(quote => {
+            const quoteEl = document.createElement('blockquote');
+            quoteEl.classList.add('quote');
 
-        quoteEl.innerHTML = `
-        <span>${quote.id}</span>
-        ${quote.quote}
-        <footer>${quote.author}</footer>
-        `;
+            quoteEl.innerHTML = `
+            <span>${quote.id}</span>
+            ${quote.quote}
+            <footer>${quote.author}</footer>
+            `;
 
-        quotesEl.appendChild(quoteEl);
-    });
-};
+            quotesEl.appendChild(quoteEl);
+        });
+    };
 
 const hideLoader = () => {
     loader.classList.remove('show');
@@ -37,44 +41,58 @@ const showLoader = () => {
     loader.classList.add('show');
 };
 
-let currentPage = 1;
-
-const limit = 10;
-
-let total = 0;
 
 const hasMoreQuotes = (page, limit, total) => {
     const startIndex = (page - 1) * limit + 1;
-    return total === 0 || stratIndex < total;
+    return total === 0 || startIndex < total;
 }
 
 
 // load Quotes
-const loadQuotes = async (page, limit) => {
+    const loadQuotes = async (page, limit) => {
     // show the loader
-    showLoader();
+        showLoader();
 
-    // to make sure that the loading indicator is always showing, add setTimeout() function
+        // to make sure that the loading indicator is always showing, add setTimeout() function
 
-    // 0.5 second later
-    setTimeout(async () => {
-        try {
-            // if having more quotes to fetch
-            if (hasMoreQuotes(page, limit, total)) {
-                // call the API to get quotes
-                const response = await getQuotes(page, limit);
+        // 0.5 second later
+        setTimeout(async () => {
+            try {
+                // if having more quotes to fetch
+                if (hasMoreQuotes(page, limit, total)) {
+                    // call the API to get quotes
+                    const response = await getQuotes(page, limit);
 
-                // show quotes
-                showQuotes(response.data);
+                    // show quotes
+                    showQuotes(response.data);
 
-                // update the total
-                total = response.total;
+                    // update the total
+                    total = response.total;
+                }
+            } catch (error) {
+                console.log(error.message);
+            } finally {
+                hideLoader();
             }
-        } catch (error) {
-            console.log(error.message);
-        } finally {
-            hideLoader();
-        }
-    }, 500);
+        }, 500);
 
-};
+    };
+
+    // control variables
+    let currentPage = 1;
+    const limit = 10;
+    let total = 0;
+
+    window.addEventListener("scroll", () => {
+        const { scrollTop, scrollHeight, clientHeight} = document.documentElement;
+
+        if ((scrollTop + clientHeight >= scrollHeight - 5) && hasMoreQuotes(currentPage, limit, total)) {
+            currentPage++;
+            loadQuotes(currentPage, limit);
+        }}, {
+        passive: false
+    });
+
+    // initialize
+    loadQuotes(currentPage, limit);
+})();
